8ef19f78c3d8a385f81bd0bbe1bb94554107b49b
Dante J. Anjos <dantewebmaster@MBP-de-Dante.home>
2020-01-09 01:31:48 -0300

    Simple redux saga test

--- src/__tests__/store/ducks/users/sagas.test.tsx
+++ src/__tests__/store/ducks/users/sagas.test.tsx
@@ -1,5 +1,6 @@
 import { call } from 'redux-saga/effects';
 import { loadUsers } from '../../../../store/ducks/users/sagas';
+import { getUsers } from '../../../../services/users.service';
 
 describe('testing sagas - function calls', () => {
   let generator = null;
@@ -8,8 +9,8 @@ describe('testing sagas - function calls', () => {
     generator = loadUsers();
   });
 
-  test('should call the loadUsers function', () => {
-      const expected = call(loadUsers);
+  test('should call the getUsers function', () => {
+      const expected = call(getUsers);
       const actual = generator.next();
 
       expect(actual.value).toEqual(expected);
